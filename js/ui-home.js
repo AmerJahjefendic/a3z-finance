ab2e14165d7052636086ba53738d9acc31aba7fe
Amer <amer@cybermetis.io>
2025-12-10 22:44:26 +0100

    Finaliziran minimalistički Dashboard i Bubble Navigacija

--- js/ui-home.js
+++ js/ui-home.js
@@ -1,26 +1,43 @@
 import { recalc } from "./calc.js";
 import { renderTransactionForm, renderTransactionList } from "./ui.js";
 
+
 export function renderHome() {
     const root = document.getElementById("home");
 
     root.innerHTML = `
-        <div class="card">
-            <h2>Troškovi i Uplate</h2>
+        <h2>Finansijski pregled</h2>
+        
+        <div class="dashboard-grid">
+            
+            <div class="card summary-card income-bg">
+                <h3>Prihod</h3>
+                <p>Ukupno:</p>
+                <h1 id="totalIncome">0 KM</h1>
+            </div>
+
+            <div class="card summary-card expense-bg">
+                <h3>Trošak</h3>
+                <p>Ukupno:</p>
+                <h1 id="totalExpense">0 KM</h1>
+            </div>
+
+            <div class="card summary-card profit-bg">
+                <h3>Neto Profit</h3>
+                <p>Razlika:</p>
+                <h1 id="netProfit">0 KM</h1>
+            </div>
+        </div>
+
+        <div class="card details-card">
+            <h4>Troškovi po osobi</h4>
             <p>Amer uplatio: <b><span id="amerPaid">0</span> KM</b></p>
             <p>Emir uplatio: <b><span id="emirPaid">0</span> KM</b></p>
-            <p>Ukupan trošak: <b><span id="totalExpense">0</span> KM</b></p>
             <p>Trošak po osobi: <b><span id="expensePerPerson">0</span> KM</b></p>
-            <p id="expenseBalanceText" class="highlight"></p>
         </div>
 
-        <div class="card">
-            <h2>Prihodi i Dobit</h2>
-            <p>Prihod: <b><span id="totalIncome">0</span> KM</b></p>
-            <p>Razlika (profit): <b><span id="netProfit">0</span> KM</b></p>
-            <p>Dobit po osobi: <b><span id="profitPerPerson">0</span> KM</b></p>
-        </div>
-
+        <p id="expenseBalanceText" class="balance-highlight"></p>
+        
         <div id="transactionForm"></div>
 
         <div class="card">
